1b96aa488ccd23d628bee836acc301b8b6442c42
Chris Behr <chrisbehr@Chriss-MacBook-Pro.local>
2024-10-02 11:25:48 +0100

    Added sitemap and fixed the cta box

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://usedream.ai',
+    integrations: [sitemap({
+        customPages: ['https://www.usedream.ai/use-cases/new-territory?lang=fr', 'https://www.usedream.ai/use-cases/new-territory?lang=de', 'https://www.usedream.ai/use-cases/new-territory?lang=es', 'https://www.usedream.ai/use-cases/new-territory?lang=se'],
+      })],
+});
