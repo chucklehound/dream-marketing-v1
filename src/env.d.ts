865a6cc919358acabf2317256bcc4221895384a9
Chris Behr <chrisbehr@MacBook-Pro.local>
2025-09-07 20:33:34 +0100

    Added a subtle background texture to the homepage

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare global {
+  interface Window {
+    __dreamTicker?: {
+      start: () => void;
+      stop: () => void;
+    } | undefined;
+  }
+}
+
+export {};
